5891da68db04c1aae6eaff298d919b42cf2b04c4
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-08 11:24:10 +0000

    atualização de parametros

--- trunk/contest/Give Me Some Credit/program/nnetboost/mabNnetTrain.m
+++ trunk/contest/Give Me Some Credit/program/nnetboost/mabNnetTrain.m
@@ -1,6 +1,6 @@
 function [nn_model] = mabNnetTrain(X, y, weigths, ~)
 
-    nn_config = newff(minmax(X), minmax(y), 30, {'tansig', 'tansig', 'tansig'});
+    nn_config = newff(minmax(X), minmax(y), 15, {'tansig', 'tansig', 'tansig'});
     nn_config.trainParam.max_fail = 60;
     nn_config.trainParam.min_grad = 1e-30;
     
