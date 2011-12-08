2400dfe69904513c7decd5857a69a09c7c0ad274
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-08 19:32:00 +0000

    correção na estimativa dos dados não presentes usando nearest neighboor

--- contest/Give Me Some Credit/program/nnet/nnetTrain.m
+++ contest/Give Me Some Credit/program/nnet/nnetTrain.m
@@ -1,7 +1,7 @@
 function [nn_model] = nnetTrain(X, y, tp, af)
     
     nn_config = newff(minmax(X), minmax(y), tp, af);
-    nn_config.trainParam.max_fail = 60;
+    nn_config.trainParam.max_fail = 30;
     nn_config.trainParam.min_grad = 1e-30;
     
     w = ones(size(y));
--- contest/Give Me Some Credit/program/nnet/nnetTrainMain.m
+++ contest/Give Me Some Credit/program/nnet/nnetTrainMain.m
@@ -4,12 +4,14 @@ fprintf('Loading data...\n');
 use_pca = 0;
 if (use_pca)
     load('../data/binaryPCAData.mat'); % Z_train y_train Z_val y_val Z_test y_test
-    X_train = Z_train; X_val = Z_val; X_test = Z_test;
+    X_train = Z_train'; X_val = Z_val'; X_test = Z_test';
     load binaryNnetModelPCAData.mat; % nn_model nn_model_auc
 else
     load('../data/binaryData.mat'); % X_train y_train X_val y_val X_test y_test
+    X_train = X_train'; X_val = X_val'; X_test = X_test';
     load binaryNnetModelData.mat; % nn_model nn_model_auc
 end
+y_train = y_train'; y_val = y_val'; y_test = y_test';
 
 n = 5;
 model_hist = cell(n, 1);
@@ -18,7 +20,7 @@ for i=1:n
     % Training
     tic;
     fprintf('Model nnet %d of %d train...\n', i, n);
-    [cur_nn_model] = nnetTrain([X_train X_val], [y_train y_val], 30, {'tansig', 'tansig'});
+    [cur_nn_model] = nnetTrain([X_train X_val], [y_train y_val], 15, {'tansig', 'tansig'});
     [curr_nn_model_auc, ~] = nnetEval(cur_nn_model, X_train, y_train, X_val, y_val, X_test, y_test);
     
     % Saving
