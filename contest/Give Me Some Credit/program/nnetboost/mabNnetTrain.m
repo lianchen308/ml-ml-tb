61b2a93e770d238f0a034d41d66cbac68d23e078
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-09 03:57:48 +0000

    geração de um modelo melhor

--- contest/Give Me Some Credit/program/nnetboost/mabNnetTrain.m
+++ contest/Give Me Some Credit/program/nnetboost/mabNnetTrain.m
@@ -5,9 +5,7 @@ function [nn_model] = mabNnetTrain(X, y, weigths, ~)
     nn_config.trainParam.min_grad = 1e-30;
     
     weigths_ratio = ones(size(y));
-    ratio = length(find(y==1))/length(y);
-    weigths_ratio(y==-1) = 1/(1-ratio);
-    weigths_ratio(y==1) = (1/ratio);
+    weigths_ratio(y==1) = length(find(y==-1))/length(find(y==1));
 
     [nn_model] = {train(nn_config, X, y, [], [], weigths.*weigths_ratio)};
     
