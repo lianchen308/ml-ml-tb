ea62b195e5399d6938294ddbbce0e8d826b92184
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-14 20:18:14 +0000

    treinamento de um modelo melhor do adaboost

--- trunk/contest/Give Me Some Credit/program/mix/nnetTrain.m
+++ trunk/contest/Give Me Some Credit/program/mix/nnetTrain.m
@@ -1,17 +1,17 @@
 function [nn_model] = nnetTrain(X, y, weigths_or_iteration, ~)
     X = X';
     y = y';
-    nn_config = newff(minmax(X), minmax(y), [30 30], {'tansig', 'tansig', 'tansig'});
-    nn_config.trainParam.max_fail = 3;
+    nn_config = newff(minmax(X), minmax(y), [30 10], {'tansig', 'tansig', 'tansig'});
+    nn_config.trainParam.max_fail = 5;
     nn_config.trainParam.min_grad = 1e-30;
     nn_config.divideFcn = 'divideblock';
     nn_config.divideParam.trainRatio = 0.6; 
-    nn_config.divideParam.valRatio = 0.25;
-    nn_config.divideParam.testRatio = 0.15;
+    nn_config.divideParam.valRatio = 0.30;
+    nn_config.divideParam.testRatio = 0.10;
     
     %nn_weigths = ones(size(y));
     nn_weigths = deftrainweight(y); % ones(size(y)); 
-    nn_weigths(y == 1) = nn_weigths(y == 1)*0.8;
+    nn_weigths(y == 1) = nn_weigths(y == 1)*0.9;
     
     if (~isscalar(weigths_or_iteration))
         nn_weigths = nn_weigths.*weigths_or_iteration';
--- trunk/util/rf-util/rfeval.m
+++ trunk/util/rf-util/rfeval.m
@@ -1,16 +1,26 @@
-% [y_test_auc, y_test_acc] = rfeval(rf_model, X_train, y_train, X_val, y_val, X_test, y_test)
-function [y_test_auc, y_test_acc] = rfeval(rf_model, X_train, y_train, X_val, y_val, X_test, y_test)
+% [y_test_acc, y_test_auc, y_val_acc, y_val_auc, y_train_acc, y_train_auc, y_all_acc, y_all_auc] = ...
+%    rfeval(rf_model, X_train, y_train, X_val, y_val, X_test, y_test)
+function [y_test_acc, y_test_auc, y_val_acc, y_val_auc, y_train_acc, y_train_auc, y_all_acc, y_all_auc] = ...
+    rfeval(rf_model, X_train, y_train, X_val, y_val, X_test, y_test)
 
     fprintf('Evaluating random forest...\n');
-    [~, ~, y_train_acc, y_train_auc] = rfpredict(rf_model, X_train, y_train);
+    [y_train_pred, ~, y_train_acc, y_train_auc] = rfpredict(rf_model, X_train, y_train);
     fprintf('Train result: acc = %1.4f, auc = %1.4f\n', y_train_acc, y_train_auc);
     
-    [~, ~, y_val_acc, y_val_auc] = rfpredict(rf_model, X_val, y_val);
+    [y_val_pred, ~, y_val_acc, y_val_auc] = rfpredict(rf_model, X_val, y_val);
     fprintf('Val result: acc = %1.4f, auc = %1.4f\n', y_val_acc, y_val_auc);
     
     [y_test_pred, ~, y_test_acc, y_test_auc] = rfpredict(rf_model, X_test, y_test);
-    fprintf('Test result: acc = %1.4f, auc = %1.4f\n', y_test_acc, y_test_auc);
     [y_test_negacc, y_test_posacc] = classaccuracy(y_test, y_test_pred);
-    fprintf('Test acc: neg acc = %1.4f, pos acc = %1.4f\n', y_test_negacc, y_test_posacc);
+    fprintf('Test result: acc = %1.4f, neg acc = %1.4f, pos acc = %1.4f, auc = %1.4f\n', ...
+        y_test_acc, y_test_negacc, y_test_posacc, y_test_auc);
+    
+    y_all = [y_train; y_val; y_test];
+    y_all_pred = [y_train_pred; y_val_pred; y_test_pred];
+    y_all_acc = accuracy(y_all, y_all_pred);
+    y_all_auc = aucscore(y_all, y_all_pred);
+    [y_all_negacc, y_all_posacc] = classaccuracy(y_all, y_all_pred);
+    fprintf('All result: acc = %1.4f, neg acc = %1.4f, pos acc = %1.4f, auc = %1.4f\n', ...
+        y_all_acc, y_all_negacc, y_all_posacc, y_all_auc);
 
 end
