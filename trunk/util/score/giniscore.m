4d4e5cd67c01fc439bb22bfe960296890a90ed7d
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-15 13:13:38 +0000

    inclusão do código para calcular o gini e mudança de diretório

---
+++ trunk/util/score/giniscore.m
@@ -0,0 +1,24 @@
+function score = giniscore(y, y_pred)
+%--------------------------------------------------------------------------
+%
+% This function calculates the non-normalized gini index. To normalize,
+% divide result by ginicalc(y,y).
+%
+% gini=giniscore(y,y_pred)
+%
+% y = an [n,1] array of actual probabilities
+%
+% y_pred = an [n,1] array of predicted probabilities
+%
+% gini is the non-normalized gini index. Higher values indicate
+% that the probabilities in y_pred are a better fit to y.
+%
+%--------------------------------------------------------------------------
+	y=y(:);
+	y_pred=y_pred(:);
+
+	n=length(y);
+	k=[y,y_pred,[1:n]'];
+	k=sortrows(k,[-2,3]);
+	gini=sum(cumsum(k(:,1)./sum(k(:,1)))-[1/n:1/n:1]')/n;
+end
