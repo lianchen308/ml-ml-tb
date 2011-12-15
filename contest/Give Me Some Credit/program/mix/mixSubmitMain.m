36e9e05ace870ba3b2d4744a4d33a32aa6ac462c
lucas.eustaquio <lucas.eustaquio@0f98234a-6ff7-de43-94df-4f1f2ddba008>
2011-12-15 00:56:41 +0000

    inclusão da geração da resposta por tandom forest

--- contest/Give Me Some Credit/program/mix/mixSubmitMain.m
+++ contest/Give Me Some Credit/program/mix/mixSubmitMain.m
@@ -2,6 +2,7 @@ clc; clear;
 fprintf('Loading submission data...\n');
 % loading data
 load binaryMixBoostModelData; 
+load binaryMixRfModelData.mat;
 load binarySubmitMixData.mat;
 
 % Saving submit
@@ -12,3 +13,11 @@ fprintf('Saving mixed adaboost submission data...\n');
 dlmwrite('csvSubmitMixBoostData.csv',[(1:length(y_submit_prob))' y_submit_prob], ...
     'delimiter',',','precision',6);
 fprintf('mixed adaboost saved...\n');
+
+% Saving submit
+fprintf('Running mixed rf model on submission data...\n');
+[~, y_submit_prob] = rfpredict(mix_rf_model, X_submit_mix(:, 11:end));
+fprintf('Saving rf submission data...\n');
+dlmwrite('csvSubmitMixRfData.csv',[(1:length(y_submit_prob))' y_submit_prob], ...
+    'delimiter',',','precision',6);
+fprintf('mixed rf saved...\n');
